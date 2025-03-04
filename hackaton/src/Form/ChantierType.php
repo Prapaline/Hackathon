112fc3ce0891e0b227d2b66dcbf79f05fd42e871
RafikiDeFaritas <119421045+RafikiDeFaritas@users.noreply.github.com>
2025-03-04 17:48:23 +0100

    Maj forms

--- hackaton/src/Form/ChantierType.php
+++ hackaton/src/Form/ChantierType.php
@@ -6,6 +6,10 @@ use App\Entity\Chantier;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,22 +18,67 @@ class ChantierType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('location')
-            ->add('description')
-            ->add('status')
-            ->add('start_date', null, [
-                'widget' => 'single_text',
+            ->add('name', TextType::class, [
+                'label' => 'Nom du chantier',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Entrez le nom du chantier',
+                ],
             ])
-            ->add('end_date', null, [
-                'widget' => 'single_text',
+            ->add('location', TextType::class, [
+                'label' => 'Localisation',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Entrez la localisation du chantier',
+                ],
             ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Décrivez le chantier',
+                    'rows' => 4,
+                ],
+            ])
+            ->add('status', ChoiceType::class, [
+                'label' => 'Statut',
+                'choices' => [
+                    'En cours' => 'En cours',
+                    'Terminé' => 'Terminé',
+                    'En attente' => 'En attente',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('start_date', DateType::class, [
+                'label' => 'Date de début',
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('end_date', DateType::class, [
+                'label' => 'Date de fin',
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            // ->add('users', EntityType::class, [
+            //     'class' => User::class,
+            //     'choice_label' => 'id',
+            //     'multiple' => true,
+            // ])
             ->add('users', EntityType::class, [
+                'label' => 'Utilisateurs',
                 'class' => User::class,
-                'choice_label' => 'id',
+                'choice_label' => 'id', 
                 'multiple' => true,
-            ])
-        ;
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- hackaton/src/Form/UserType.php
+++ hackaton/src/Form/UserType.php
@@ -9,6 +9,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 
 class UserType extends AbstractType
 {
@@ -16,7 +18,15 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'ROLE_ADMIN' => 'ROLE_ADMIN',
+                    'ROLE_USER' => 'ROLE_USER',
+                    // Ajoutez d'autres rôles si nécessaire
+                ],
+                'multiple' => true, // Permet de sélectionner plusieurs rôles
+                'expanded' => true, // Affiche des cases à cocher
+            ])
             ->add('password')
             ->add('first_name')
             ->add('last_name')
@@ -29,8 +39,7 @@ class UserType extends AbstractType
                 'class' => Chantier::class,
                 'choice_label' => 'id',
                 'multiple' => true,
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
